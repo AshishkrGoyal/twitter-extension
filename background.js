db88ac81f0224f49b9bd27e5408bc2a9b3ad0dc5
AshishkrGoyal <15103140ashish@gmail.com>
2017-09-25 07:01:24 +0530

    all-sharing-added-:chart_with_upwards_trend:

--- background.js
+++ background.js
@@ -1,14 +1,48 @@
 /*alert();*/
-chrome.contextMenus.create({
-    title: "share it to twitter",
-    contexts: ["selection"],
-    onclick: myFunc
-});
+
+var contextMenu = ["selection", "link", "image", "page"];
+
+for(var i=0; i<contextMenu.length; i++)
+{
+    var selected_context = contextMenu[i];
+    chrome.contextMenus.create({
+        title: "share "+ contextMenu[i] +" to twitter",
+        contexts: [contextMenu[i]],
+        onclick: myFunc,
+        id: selected_context
+    });
+
+}
 
 function myFunc(selected) {
     // alert(selected.selectionText);
     //alert("you want to share it !!")
-    chrome.tabs.create({
+
+    switch(selected.menuItemId)
+    {
+        case 'selection':
+            chrome.tabs.create({
+                url:"https://twitter.com/intent/tweet?text="+selected.selectionText
+            });
+            break;
+        case 'link':
+            chrome.tabs.create({
+                url:"https://twitter.com/intent/tweet?url="+selected.linkUrl
+            });
+            break;
+        case 'image':
+            chrome.tabs.create({
+                url:"https://twitter.com/intent/tweet?url="+selected.srcUrl
+            });
+            break;
+        case 'page':
+            chrome.tabs.create({
+                url:"https://twitter.com/intent/tweet?text="+selected.selectionText
+            });
+            break;
+
+    }
+   /* chrome.tabs.create({
         url:"https://twitter.com/intent/tweet?text="+selected.selectionText
-    })
+    })*/
 }
